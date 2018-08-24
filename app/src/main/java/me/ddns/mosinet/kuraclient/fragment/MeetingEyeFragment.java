093b0fbcabae162a47e512d5d441e28b46c58cd1
mosomate <TeLeporter95>
2018-08-24 21:27:56 +0200

    I think it's done

--- app/src/main/java/me/ddns/mosinet/kuraclient/fragment/MeetingEyeFragment.java
+++ app/src/main/java/me/ddns/mosinet/kuraclient/fragment/MeetingEyeFragment.java
@@ -7,14 +7,18 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import me.ddns.mosinet.kuraclient.MainActivity;
@@ -64,39 +68,140 @@ public class MeetingEyeFragment extends Fragment{
         // Tilting
         tiltLeftButton = view.findViewById(R.id.tiltLeftButton);
         tiltLeftButton.setEnabled(false);
+        tiltLeftButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendSingleParamCommand("tilt", "left");
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        sendSingleParamCommand("tilt", "stop");
+                        return true;
+                }
+                return false;
+            }
+        });
+
         tiltUpButton = view.findViewById(R.id.tiltUpButton);
         tiltUpButton.setEnabled(false);
+        tiltUpButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendSingleParamCommand("tilt", "up");
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        sendSingleParamCommand("tilt", "stop");
+                        return true;
+                }
+                return false;
+            }
+        });
+
         tiltRightButton = view.findViewById(R.id.tiltRightButton);
         tiltRightButton.setEnabled(false);
+        tiltRightButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendSingleParamCommand("tilt", "right");
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        sendSingleParamCommand("tilt", "stop");
+                        return true;
+                }
+                return false;
+            }
+        });
+
         tiltDownButton = view.findViewById(R.id.tiltDownButton);
         tiltDownButton.setEnabled(false);
+        tiltDownButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendSingleParamCommand("tilt", "down");
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        sendSingleParamCommand("tilt", "stop");
+                        return true;
+                }
+                return false;
+            }
+        });
 
         // Zoom
         zoomInButton = view.findViewById(R.id.zoomInButton);
         zoomInButton.setEnabled(false);
+        zoomInButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendSingleParamCommand("zoom", "in");
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        sendSingleParamCommand("zoom", "stop");
+                        return true;
+                }
+                return false;
+            }
+        });
+
         zoomOutButton = view.findViewById(R.id.zoomOutButton);
         zoomOutButton.setEnabled(false);
+        zoomOutButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        sendSingleParamCommand("zoom", "out");
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        sendSingleParamCommand("zoom", "stop");
+                        return true;
+                }
+                return false;
+            }
+        });
 
         // Presets
         preset1Button = view.findViewById(R.id.preset1Button);
         preset1Button.setEnabled(false);
+        preset1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSingleParamCommand("preset", 1);
+            }
+        });
+
         preset2Button = view.findViewById(R.id.preset2Button);
         preset2Button.setEnabled(false);
+        preset2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSingleParamCommand("preset", 2);
+            }
+        });
+
         preset3Button = view.findViewById(R.id.preset3Button);
         preset3Button.setEnabled(false);
+        preset3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSingleParamCommand("preset", 3);
+            }
+        });
 
         // Create callback for power on
         powerOnListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Create map for metrics
-                Map<String, Object> metrics = new HashMap<>();
-
-                // Add power on command
-                metrics.put("command", "power");
-                metrics.put("param", "on");
-
-                ((MainActivity) getActivity()).getMqttClient().sendMessageToDevice("meeting_eye", metrics);
+                sendSingleParamCommand("power", "on");
             }
         };
 
@@ -104,14 +209,7 @@ public class MeetingEyeFragment extends Fragment{
         powerOffListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Create map for metrics
-                Map<String, Object> metrics = new HashMap<>();
-
-                // Add power on command
-                metrics.put("command", "power");
-                metrics.put("param", "off");
-
-                ((MainActivity) getActivity()).getMqttClient().sendMessageToDevice("meeting_eye", metrics);
+                sendSingleParamCommand("power", "off");
             }
         };
 
@@ -122,69 +220,73 @@ public class MeetingEyeFragment extends Fragment{
         updateUI();
     }
 
-    public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+    public void messageArrived(String topic, MqttMessage mqttMessage) {
         Log.w("messageArrived",mqttMessage.toString());
 
         long currentSystemTime = System.currentTimeMillis();
 
-        JSONObject message = new JSONObject(mqttMessage.toString());
         Log.i("json", topic);
 
-        if (topic.equals(getContext().getString(R.string.topic_prefix, "meeting_eye/status/power"))) {
-            String powerStatus = message.getJSONObject("metrics").getString("value");
-            //Log.i("json", powerStatus);
-            if (powerStatus.equals("on")) {
-                powerButton.setText(R.string.meeting_eye_power_off_title);
-                powerButton.setOnClickListener(powerOffListener);
+        try {
+            Map<String, Object> metrics = getMetricsFromMessage(mqttMessage);
 
-                tiltLeftButton.setEnabled(true);
-                tiltUpButton.setEnabled(true);
-                tiltRightButton.setEnabled(true);
-                tiltDownButton.setEnabled(true);
+            if (topic.equals(getContext().getString(R.string.topic_prefix, "meeting_eye/status/power"))) {
+                //Log.i("json", powerStatus);
+                String powerStatus = (String)metrics.get("value");
+                if (powerStatus.equals("on")) {
+                    powerButton.setText(R.string.meeting_eye_power_off_title);
+                    powerButton.setOnClickListener(powerOffListener);
 
-                zoomInButton.setEnabled(true);
-                zoomOutButton.setEnabled(true);
+                    tiltLeftButton.setEnabled(true);
+                    tiltUpButton.setEnabled(true);
+                    tiltRightButton.setEnabled(true);
+                    tiltDownButton.setEnabled(true);
 
-                preset1Button.setEnabled(true);
-                preset2Button.setEnabled(true);
-                preset3Button.setEnabled(true);
-            }
-            else {
-                powerButton.setText(R.string.meeting_eye_power_on_title);
-                powerButton.setOnClickListener(powerOnListener);
+                    zoomInButton.setEnabled(true);
+                    zoomOutButton.setEnabled(true);
 
-                tiltLeftButton.setEnabled(false);
-                tiltUpButton.setEnabled(false);
-                tiltRightButton.setEnabled(false);
-                tiltDownButton.setEnabled(false);
+                    preset1Button.setEnabled(true);
+                    preset2Button.setEnabled(true);
+                    preset3Button.setEnabled(true);
+                } else {
+                    powerButton.setText(R.string.meeting_eye_power_on_title);
+                    powerButton.setOnClickListener(powerOnListener);
 
-                zoomInButton.setEnabled(false);
-                zoomOutButton.setEnabled(false);
+                    tiltLeftButton.setEnabled(false);
+                    tiltUpButton.setEnabled(false);
+                    tiltRightButton.setEnabled(false);
+                    tiltDownButton.setEnabled(false);
 
-                preset1Button.setEnabled(false);
-                preset2Button.setEnabled(false);
-                preset3Button.setEnabled(false);
+                    zoomInButton.setEnabled(false);
+                    zoomOutButton.setEnabled(false);
+
+                    preset1Button.setEnabled(false);
+                    preset2Button.setEnabled(false);
+                    preset3Button.setEnabled(false);
+                }
+            } else if (topic.equals(getContext().getString(R.string.topic_prefix, "meeting_eye/status/preset"))) {
+                int preset = (int)metrics.get("value");
+
+                // Clear all preset background color filter
+                preset1Button.getBackground().clearColorFilter();
+                preset2Button.getBackground().clearColorFilter();
+                preset3Button.getBackground().clearColorFilter();
+
+                switch (preset) {
+                    case 1:
+                        preset1Button.getBackground().setColorFilter(getContext().getColor(R.color.buttonHighLightColor), PorterDuff.Mode.MULTIPLY);
+                        break;
+                    case 2:
+                        preset2Button.getBackground().setColorFilter(getContext().getColor(R.color.buttonHighLightColor), PorterDuff.Mode.MULTIPLY);
+                        break;
+                    case 3:
+                        preset3Button.getBackground().setColorFilter(getContext().getColor(R.color.buttonHighLightColor), PorterDuff.Mode.MULTIPLY);
+                        break;
+                }
             }
         }
-        else if (topic.equals(getContext().getString(R.string.topic_prefix, "meeting_eye/status/preset"))) {
-            int preset = message.getJSONObject("metrics").getInt("value");
-
-            // Clear all preset background color filter
-            preset1Button.getBackground().clearColorFilter();
-            preset2Button.getBackground().clearColorFilter();
-            preset3Button.getBackground().clearColorFilter();
-
-            switch (preset) {
-                case 1:
-                    preset1Button.getBackground().setColorFilter(getContext().getColor(R.color.buttonHighLightColor), PorterDuff.Mode.MULTIPLY);
-                    break;
-                case 2:
-                    preset2Button.getBackground().setColorFilter(getContext().getColor(R.color.buttonHighLightColor), PorterDuff.Mode.MULTIPLY);
-                    break;
-                case 3:
-                    preset3Button.getBackground().setColorFilter(getContext().getColor(R.color.buttonHighLightColor), PorterDuff.Mode.MULTIPLY);
-                    break;
-            }
+        catch (JSONException e) {
+            Log.i("json", "Error occured while parsin received message: " + e.getMessage());
         }
     }
 
@@ -197,5 +299,34 @@ public class MeetingEyeFragment extends Fragment{
         if (powerButton != null) {
             powerButton.setEnabled(uiEnabled);
         }
+    }
+
+    private void sendSingleParamCommand(String command, Object value) {
+        // Create map for metrics
+        Map<String, Object> metrics = new HashMap<>();
+
+        // Add power on command
+        metrics.put("command", command);
+        metrics.put("param", value);
+
+        ((MainActivity) getActivity()).getMqttClient().sendMessageToDevice("meeting_eye", metrics);
+    }
+
+    private Map<String,Object> getMetricsFromMessage(MqttMessage mqttMessage) throws JSONException {
+        JSONObject message = new JSONObject(mqttMessage.toString());
+
+        JSONArray jsonMetrics = message.getJSONArray("metrics");
+
+        HashMap<String, Object> metrics = new HashMap<String, Object>();
+        for (int i = 0; i < jsonMetrics.length(); i++) {
+            JSONObject j = jsonMetrics.getJSONObject(i);
+            Iterator it = j.keys();
+            while (it.hasNext()) {
+                String n = (String)it.next();
+                metrics.put(n, j.get(n));
+            }
+        }
+
+        return metrics;
     }
 }
